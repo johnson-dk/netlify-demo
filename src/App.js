5d56e01d7a17b46eeed0a4bac773c6b18aaa86b9
Brian Johnson <bjohnson@grundfos.com>
2020-10-05 15:12:23 +0200

    Someting Great!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Another Awesome Feature!
+        Something Great!
       </header>
     </div>
   );
